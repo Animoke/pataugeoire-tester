ae746457aeb1f2e776ad4dfeff6c66971ac39332
gpatingr <gpatingr@student.42.fr>
2021-02-14 19:49:12 +0100

    adding colors.sh and dspl.sh

---
+++ srcs/var/colors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ############################################################################ #
+#                                                                              #
+#    piscine-ultimate-tester                                                   #
+#    v. 1.0                                                                    #
+#                                                                              #
+#    colors.sh                                      by Animoke (animoke.dev)   #
+#                                                                              #
+# ############################################################################ #
+
+# Colors variables
+
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+NOCOLOR='\033[0m'
+BLACK='\033[30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
---
+++ srcs/var/dspl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ############################################################################ #
+#                                                                              #
+#    piscine-ultimate-tester                                                   #
+#    v. 1.0                                                                    #
+#                                                                              #
+#    dspl.sh                                        by Animoke (animoke.dev)   #
+#                                                                              #
+# ############################################################################ #
+
+# Display variables
+
+uni_success=" ${GREEN}✓${NOCOLOR} "
+uni_fail=" ${RED}✗${NOCOLOR} "
+uni_arrow="➥ "
+uni_sep="❯ "
+uni_rev_sep=" ❮ "
+diff_ok=" ${GREEN}[OK]${NOCOLOR} "
+diff_ko=" ${RED}[KO]${NOCOLOR} "
